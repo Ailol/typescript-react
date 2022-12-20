57dd9fb96da9e733f4339bbffa6fb3c14362ccad
Ailo <ailonordsletta@gmail.com>
2022-12-20 18:45:01 +0100

    error push

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,6 @@ import "./assets/Form.css";
 import { Button } from "@mui/material";
 import BusTable from "./components/Bus/BusTable";
 import RegisterForm from "./components/Form/RegisterForm";
-import Register from "./components/Form/Register";
-
 const App: React.FC = () => {
     const [tabBus, setBusTab] = useState<boolean>(false);
 
@@ -13,7 +11,7 @@ const App: React.FC = () => {
         <div className="container">
             <Button onClick={() => setBusTab(false)}> Registrèr</Button>
             <Button onClick={() => setBusTab(true)}> Bus tavla</Button>
-            <Register />
+
             {!tabBus ? <RegisterForm /> : <BusTable />}
         </div>
     );
